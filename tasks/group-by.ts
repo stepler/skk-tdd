8c2889d04b0183a1d537d58013aeaab89c3abc20
Kseniia Samoilova <skk1020@yandex-team.ru>
2022-04-08 13:50:14 +0300

    var 3

--- tasks/group-by.ts
+++ tasks/group-by.ts
@@ -1,15 +1,17 @@
 type ArrPred<T> = (x: T, index: number) => boolean;
 
 export function groupBy<T = any>(fns: ArrPred<T>[], arr: T[]): T[][] {
-    fns.push(() => true);
-
-    const res: T[][] = new Array(fns.length);
+    const res: T[][] = new Array(fns.length + 1);
     for (let j = 0; j < res.length; j++) {
         res[j] = [];
     }
 
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < res.length; j++) {
+            if (! fns[j]) {
+                res[j].push(arr[i]);
+                break;
+            }
             if (fns[j](arr[i], i)) {
                 res[j].push(arr[i]);
                 break;
