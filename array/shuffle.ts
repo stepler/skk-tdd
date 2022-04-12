8403221990687f3a16f12c9b6d9b7d692047115b
Kseny Samoylova <skk1020@yandex.ru>
2022-04-12 08:10:59 +0300

    solutions in new folder

--- array/shuffle.ts
+++ array/shuffle.ts
@@ -3,5 +3,15 @@
  */
 
 export function shuffle<T = any>(list: T[]): T[] {
-    //...
+    const res = new Array(list.length);
+    res[0] = list[0];
+
+    for (let i = 1; i < list.length; i++) {
+        const idx = Math.floor(Math.random() * i);
+        const swap = res[idx];
+        res[idx] = list[i];
+        res[i] = swap;
+    }
+    
+    return res;
 }
