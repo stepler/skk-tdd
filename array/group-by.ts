8adb5fe3e890212fdc739201d25facda6ace08ae
Kseny Samoylova <skk1020@yandex.ru>
2022-04-12 08:10:59 +0300

    solutions in new folder

--- array/group-by.ts
+++ array/group-by.ts
@@ -9,5 +9,22 @@
 type ArrPred<T> = (x: T, index: number) => boolean;
 
 export function groupBy<T = any>(fns: ArrPred<T>[], list: T[]): T[][] {
-    //...
+    const res: T[][] = new Array(fns.length + 1);
+    for (let j = 0; j < res.length; j++) {
+        res[j] = [];
+    }
+
+    for (let i = 0; i < list.length; i++) {
+        for (let j = 0; j < res.length; j++) {
+            if (! fns[j]) {
+                res[j].push(list[i]);
+                break;
+            }
+            if (fns[j](list[i], i)) {
+                res[j].push(list[i]);
+                break;
+            }
+        }
+    }
+    return res;
 }
