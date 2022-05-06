89140be5648d8c5abaef47f36e59268fb450741d
Simon Moiseenko <stepler@yandex-team.ru>
2022-05-06 16:21:09 +0300

    Fix array/slice and array/move tests

--- array/move.test.ts
+++ array/move.test.ts
@@ -6,7 +6,7 @@ describe('move', () => {
 
         expect(move(arr, 0, 1)).toBe(arr);
 
-        expect(move([1, 2, 3, 4, 5], 2, 4)).toEqual([1, 2, 5, 3, 4]);
+        expect(move([1, 2, 3, 4, 5], 4, 2)).toEqual([1, 2, 5, 3, 4]);
 
         expect(move([1, 2, 3, 4, 5], 2, 0)).toEqual([3, 1, 2, 4, 5]);
 
@@ -23,5 +23,7 @@ describe('move', () => {
         expect(move([1, 2, 3, 4, 5], -1, 1)).toEqual([1, 5, 2, 3, 4]);
 
         expect(move([1, 2, 3, 4, 5], -4, -1)).toEqual([1, 3, 4, 5, 2]);
+
+        expect(move([1, 2, 3, 4, 5], -4, -10)).toEqual([1, 2, 3, 4, 5]);
     });
 });
--- array/slice.test.ts
+++ array/slice.test.ts
@@ -8,7 +8,9 @@ describe('slice', () => {
 
         expect(slice(list, 2)).toEqual([3, 4, 5]);
 
-        expect(slice(list, 2, 4)).toEqual([3, 4]);
+        expect(slice(list, 2, 2)).toEqual([3]);
+
+        expect(slice(list, 2, 4)).toEqual([3, 4, 5]);
 
         expect(slice(list, 10)).toEqual([]);
 
@@ -18,10 +20,14 @@ describe('slice', () => {
     it('improved', () => {
         const list = [1, 2, 3, 4, 5];
 
-        expect(slice(list, -3)).toEqual([2, 3, 4]);
+        expect(slice(list, -3)).toEqual([3, 4, 5]);
 
-        expect(slice(list, 2, -2)).toEqual([3]);
+        expect(slice(list, 2, -3)).toEqual([3]);
 
-        expect(slice(list, -4, -1)).toEqual([2, 3, 4]);
+        expect(slice(list, -4, -1)).toEqual([2, 3, 4, 5]);
+
+        expect(slice(list, -10)).toEqual([]);
+
+        expect(slice(list, 2, -10)).toEqual([]);
     });
 });
